9696b0876f1dbed9e5f05ecf9b2c13f9776b4570
Luiz Henrique de Campos Monteiro <139652963+henriquedcmonteiro@users.noreply.github.com>
2023-08-01 20:34:31 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,27 +1,40 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -u
 
-log() {
-    mkdir $(date -u +"%Y-%M-%dT%H:%M:%SZ")
-    path=$_
-    cp texto.txt "./${path}"    
+ano=$(date -u +"%Y")
+mes=$(date | cut -d " " -f 2)
+dia=$(date -u +"%d")
+
+if [[ ! -d ${ano} ]]; then
+	mkdir ${ano}
+	cd ${ano}
+else
+	cd ${ano}
+fi
+
+if [[ ! -d ${mes} ]]; then
+	mkdir ${mes}
+	cd ${mes}
+else
+	cd ${mes}
+fi
+
+if [[ ! -d ${dia} ]]; then
+	mkdir ${dia}
+	cd ${dia}
+else
+	cd ${dia}
+fi
+
+criar_arquivo() {
+	mkdir $(date -u +"%Y-%m-%dT%H:%M:%SZ")
+	path=$_
+	git clone "https://github.com/henriquedcmonteiro/special_shell_variables" 
+	mv $(basename "https://github.com/henriquedcmonteiro/special_shell_variables") ${path}
 }
 
-log 
-
-teste=$( ls | sort ) 
-teste2=$(date -u +"%M")
-
-for i in ${teste}; do
-    if [[ "${i}" > "${teste2}" ]]; then
-        echo ${i}
-        cd ../
-        mkdir nova_pasta
-        break
-    fi
-    
-done
+criar_arquivo
 
 
 
